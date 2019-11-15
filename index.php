2203158cf453c5ede68b7438df1a45b6f9c3980e
Glaynder <liker243123@gmail.com>
2019-11-15 21:33:41 +0200

    del

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mod = $_GET['mod'];
-if (!$mod || $mod == "main") {
-	include 'modules/main/index.php';
-} else {
-	include 'modules/error.php';
-}
-?>
--- modules/error.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-
-<body>
-<h1>Страница не найдена</h1>
-</body>
-
-</html>
